6481aa1a41c27aa0f056f750bf3538e0891b7969
zzangjjieeun <ssongjjieeun@gmail.com>
2024-11-27 11:54:21 +0900

    swipe ui 디자인 수정

--- frontend/src/Swipe/swipe.js
+++ frontend/src/Swipe/swipe.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import TinderCard from 'react-tinder-card';
-import './swipe.css';
+import confetti from 'canvas-confetti';
+import './swipe.css'; // CSS 파일
+import '../common.css';
 
 const db = [
     { name: 'Hamburger', url: process.env.PUBLIC_URL + '/hamburger.jpg' },
@@ -12,12 +14,19 @@ const db = [
 
 function Swipe() {
     const [characters, setCharacters] = useState(db);
-    const [lastDirection, setLastDirection] = useState(null);
 
     const swiped = (direction, nameToDelete) => {
         console.log('Removing: ' + nameToDelete);
         setCharacters((prev) => prev.filter((character) => character.name !== nameToDelete));
-        setLastDirection(direction);
+        // 오른쪽으로 스와이프 했을 때만 불꽃놀이 실행
+        if (direction === 'right') {
+            confetti({
+                particleCount: 200,
+                spread: 100,
+                origin: { x: 0.5, y: 0.6 }, // 화면 중앙에서 터짐
+                colors: ['#ff4e50', '#ff944e', '#fffdb0'], // 밝은 색상
+            });
+        }
     };
 
     const outOfFrame = (name) => {
@@ -26,7 +35,10 @@ function Swipe() {
 
     return (
         <div className="swipe-container">
-            <h1>스와이프로 밥친구 찾기</h1>
+            {/* Title 문구 */}
+            <h1 className="title">Swipe!</h1>
+            <h2 className="subtitle">and find a friend</h2>
+
             <div className="card-container">
                 {characters.map((character) => (
                     <TinderCard
@@ -44,10 +56,8 @@ function Swipe() {
                     </TinderCard>
                 ))}
             </div>
-            {lastDirection && (
-                <h2 className="info-text">You swiped {lastDirection}</h2>
-            )}
-            {/* Footer 추가 */}
+
+          {/* Footer */}
             <div className="footer">
                 <button className="btn-footer">스와이프</button>
                 <button className="btn-footer">B</button>
