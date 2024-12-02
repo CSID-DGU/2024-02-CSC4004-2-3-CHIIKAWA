c7a1046c6c36d7394e29bb404beb0cbf0d973664
Hyeok22 <wnsgur347@gmail.com>
2024-12-03 06:28:31 +0900

    group선택 시, 선택한 채팅방 정보를 넘기며 chat-box 재로드 로직 구현

--- frontend/src/ChatRoom/ChatGroup/chatgroup.js
+++ frontend/src/ChatRoom/ChatGroup/chatgroup.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 import "./chatgroup.css";
 
 const ChatGroup = ({ group }) => {
+    const navigate = useNavigate();
+
     const onClickGroup = () => {
         // 그룹 클릭 시, 실행되는 코드
         console.log(`Group${group.id} Clicked!`);
+        navigate("/chatroom");
     };
 
     return (
